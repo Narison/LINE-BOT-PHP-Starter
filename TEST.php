d9366f1ee5d1a264af5e8bbaf9853bfb76ee3a2b
Narison <30853865+Narison@users.noreply.github.com>
2017-08-15 14:34:15 +0700

    Update TEST.php

--- TEST.php
+++ TEST.php
@@ -48,92 +48,12 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['template'] = {
       $arrPostData['messages'][0]['type'] = "confirm";
       $arrPostData['messages'][0]['text'] = "Are you sure?";
-      $arrPostData['messages'][0]['actions'] = [<?php
- 
-$strAccessToken = "HnYiosVXrVsSbH35XHeQCkTgPn6Pa3shrsh+K7RJ8RIPF8hpXr4IBm40k/4B2lmr1mIRRl+JFwcohXq7JIXSmOOoBl2UhUoaMEGuRaD5uQ1kXURzsh2vwjY58D1/RPO523ZweZArgtN8XaHy5eZJvQdB04t89/1O/w1cDnyilFU=";
- 
-$content = file_get_contents('php://input');
-$arrJson = json_decode($content, true);
- 
-$strUrl = "https://api.line.me/v2/bot/message/reply";
- 
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else if($arrJson['events'][0]['message']['text'] == "ที่อยู่"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "location";
-  $arrPostData['messages'][0]['title'] = "บริษัท โปรซอฟท์ คอมเทค จำกัด";
-  $arrPostData['messages'][0]['address'] = "287 ซอย7 หมู่5 081 359 6921 เปิดจนถึง 17:45";
-  $arrPostData['messages'][0]['latitude'] = "18.8254729";
-  $arrPostData['messages'][0]['longitude'] = "99.01145250000002";
-}else if($arrJson['events'][0]['message']['text'] == "Img"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "image";
-  $arrPostData['messages'][0]['originalContentUrl'] = "https://drive.google.com/file/d/0B_jiBQxoXSnBNzVvNWpUUTRXRm8/view?usp=sharing";
-  $arrPostData['messages'][0]['previewImageUrl'] = "https://drive.google.com/file/d/0B_jiBQxoXSnBQkVVamd4YVpNVDA/view?usp=sharing";
-}else if($arrJson['events'][0]['message']['text'] == "temp"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "template";
-  $arrPostData['messages'][0]['altText'] = "this is a buttons template";
-  $arrPostData['messages'][0]['template'] = {
-      $arrPostData['messages'][0]['type'] = "confirm";
-      $arrPostData['messages'][0]['text'] = "Are you sure?";
       $arrPostData['messages'][0]['actions'] = [
           {
             $arrPostData['messages'][0]['type'] = "message";
             $arrPostData['messages'][0]['label'] = "Yes";
             $arrPostData['messages'][0]['text'] = "yes";
-          },
-          {
-            $arrPostData['messages'][0]['type'] = "message";
-            $arrPostData['messages'][0]['label'] = "No";
-            $arrPostData['messages'][0]['text'] = "no";
           }
-      ]
-  }
-}else{
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "sticker";
-  $arrPostData['messages'][0]['packageId'] = "2";
-  $arrPostData['messages'][0]['stickerId'] = "149";
-}
- 
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
- 
-?>
-
-    
           {
             $arrPostData['messages'][0]['type'] = "message";
             $arrPostData['messages'][0]['label'] = "No";
