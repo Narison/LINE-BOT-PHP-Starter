04cd7db1c7aaf443e4d5590e2abe4ae0a483c5c0
Narison <30853865+Narison@users.noreply.github.com>
2017-08-22 14:59:26 +0700

    Update TEST.php

--- TEST.php
+++ TEST.php
@@ -58,7 +58,7 @@ if($arrJson['events'][0]['message']['text'] == "ID"){
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "เมื่อกี้คุณบอกว่า" $text
+				'text' => 'เมื่อกี้คุณบอกว่า'.$text
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
