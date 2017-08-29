a1f97cccd9d64a9088b28115033e7e47e802b48f
Narison <30853865+Narison@users.noreply.github.com>
2017-08-29 13:46:01 +0700

    Create connectdb.php

---
+++ connectdb.php
@@ -0,0 +1,13 @@
+<?php
+	$objConnect = mssql_connect("localhost","NARISON\HRMI-PM2","");
+	if($objConnect)
+	{
+		echo "Database Connected.";
+	}
+	else
+	{
+		echo "Database Connect Failed.";
+	}
+
+	mssql_close($objConnect);
+?>
