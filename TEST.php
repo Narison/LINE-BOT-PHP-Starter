222bee59c63e857386271af46660c9b4f9ce37e0
Narison <30853865+Narison@users.noreply.github.com>
2017-08-30 09:00:36 +0700

    Update TEST.php

--- TEST.php
+++ TEST.php
@@ -76,15 +76,12 @@ if($arrJson['events'][0]['message']['text'] == "ID"){
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $strAccessToken);
+			
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
 			echo $result . "\r\n";
 		}
 	}
