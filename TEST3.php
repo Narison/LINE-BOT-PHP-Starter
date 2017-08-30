fba0943d1c4afc2bf6baca7d093894a5fa948155
Narison <30853865+Narison@users.noreply.github.com>
2017-08-30 17:04:52 +0700

    Update TEST3.php

--- TEST3.php
+++ TEST3.php
@@ -64,26 +64,7 @@ class LineController extends \yii\web\Controller
                 $result_text = 'ไม่พบข้อมูล';
             }
             $response_format_text = ['contentType'=>1,"toType"=>1,"text"=>$result_text];
-            
-        }else if($text_ex[0] == "อากาศ"){//ถ้าพิมพ์มาว่า อากาศ ก็ให้ไปดึง API จาก wunderground มา
-            //http://api.wunderground.com/api/yourkey/forecast/lang:TH/q/Thailand/%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3.json
-            $ch1 = curl_init();
-            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch1, CURLOPT_URL, 'http://api.wunderground.com/api/yourkey/forecast/lang:TH/q/Thailand/'.str_replace(' ', '%20', $text_ex[1]).'.json');
-            $result1 = curl_exec($ch1);
-            curl_close($ch1);
-            
-            $obj = json_decode($result1, true);
-            if(isset($obj['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric'])){
-                $result_text = $obj['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric'];
-            }else{//ถ้าไม่เจอกับตอบกลับว่าไม่พบข้อมูล
-                $result_text = 'ไม่พบข้อมูล';
-            }
-            
-            $response_format_text = ['contentType'=>1,"toType"=>1,"text"=>$result_text];
-        }else if($text == 'บอกมา'){//คำอื่นๆ ที่ต้องการให้ Bot ตอบกลับเมื่อโพสคำนี้มา เช่นโพสว่า บอกมา ให้ตอบว่า ความลับนะ
-            $response_format_text = ['contentType'=>1,"toType"=>1,"text"=>"ความลับนะ"];
+       
         }else{//นอกนั้นให้โพส สวัสดี
             $response_format_text = ['contentType'=>1,"toType"=>1,"text"=>"สวัสดี"];
         }
@@ -98,9 +79,8 @@ class LineController extends \yii\web\Controller
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json; charser=UTF-8',
-            'X-Line-ChannelID: YOUR ChannelID',
-            'X-Line-ChannelSecret: YOUR ChannelSecret',
-            'X-Line-Trusted-User-With-ACL: YOUR MID'
+            'X-Line-ChannelID:1529308085',
+            'X-Line-ChannelSecret:53580e5121a5cf757d4ea3342b91b2da',
         ));
         $result = curl_exec($ch);
         curl_close($ch);
