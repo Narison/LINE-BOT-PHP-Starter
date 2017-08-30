87597e33155f1bb881044d8e91b70ec5b4e03e4d
Narison <30853865+Narison@users.noreply.github.com>
2017-08-30 09:02:00 +0700

    Update TEST.php

--- TEST.php
+++ TEST.php
@@ -82,6 +82,8 @@ if($arrJson['events'][0]['message']['text'] == "ID"){
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 			echo $result . "\r\n";
 		}
 	}
