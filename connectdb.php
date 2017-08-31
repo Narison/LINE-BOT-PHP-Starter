888cde8b79f07632257b72223c96abff5704e0c6
Narison <30853865+Narison@users.noreply.github.com>
2017-08-31 10:33:01 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,4 +1,4 @@
-<?php
+<?
 $serverName = "serverName\43.255.240.112"; 
 $connectionInfo = array( "Database"=>"essdemo", "UID"=>"prosofthost", "PWD"=>"Hostweb$123");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
@@ -9,4 +9,4 @@ if( $conn ) {
      echo "Connection could not be established.<br />";
      die( print_r( sqlsrv_errors(), true));
 }
-?>
+
