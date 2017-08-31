44ce26df5204a2b845bab259d0661de7426963a1
Narison <30853865+Narison@users.noreply.github.com>
2017-08-31 11:29:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-echo "Hello LINE BOT";
+$username ="prosofthost";
+$password = "Hostweb$123";
+$database = "essdemo";
+$server = "43.255.240.112";
+
+$connection = mssql_connect($server, $username, $password);
+if (!$connection) {  die('Not connected : ' . mssql_get_last_message());} 
+
+
+$db_selected = mssql_select_db($databaseb, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mssql_get_last_message());
+} else{
+
+    // Success
+}
