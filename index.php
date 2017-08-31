baa29e64c76faf8b9956a1713e115c3dd25da647
Narison <30853865+Narison@users.noreply.github.com>
2017-08-31 14:59:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-$username ="prosofthost";
-$password = "Hostweb$123";
-$database = "essdemo";
-$server = "43.255.240.112";
-
-$connection = mssql_connect($server, $username, $password);
-if (!$connection) {  die('Not connected : ' . mssql_get_last_message());} 
-
-
-$db_selected = mssql_select_db($databaseb, $connection);
-if (!$db_selected) {
-  die ('Can\'t use db : ' . mssql_get_last_message());
-} else{
-
-    // Success
+echo"TEST Deploy Code Github";
 }
