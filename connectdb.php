91d0c98df6d982a9e8c2ce7dd6c07bf2ec7cae51
Narison <30853865+Narison@users.noreply.github.com>
2017-08-31 10:53:15 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "43.255.240.122\PSHOSTWEB"; 
+$serverName = "serverName\43.255.240.122\PSHOSTWEB"; 
 $connectionInfo = array( "Database"=>"essdemo", "UID"=>"prosofthost", "PWD"=>"Hostweb$123");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
