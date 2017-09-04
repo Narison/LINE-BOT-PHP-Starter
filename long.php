13992118b2f0c0d5dee2f9f77c8a287bddcff2d7
Narison <30853865+Narison@users.noreply.github.com>
2017-09-04 17:36:19 +0700

    Update and rename long.PHP to long.php

--- long.php
+++ long.php
@@ -1,3 +1,4 @@
+<?php
 $channelSecret = '53580e5121a5cf757d4ea3342b91b2da';
 $access_token  = 'HnYiosVXrVsSbH35XHeQCkTgPn6Pa3shrsh+K7RJ8RIPF8hpXr4IBm40k/4B2lmr1mIRRl+JFwcohXq7JIXSmOOoBl2UhUoaMEGuRaD5uQ1kXURzsh2vwjY58D1/RPO523ZweZArgtN8XaHy5eZJvQdB04t89/1O/w1cDnyilFU=';
  
